bec5570e08ffc862ea1ca0d2b0cdc024b77f57c7
kylwrld <danielrochinhah@gmail.com>
2024-10-27 18:53:58 -0300

    remove build sourcemaps warnings

--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,16 @@ export default defineConfig({
           "@": path.resolve(__dirname, "./src"),
         },
     },
+    build: {
+        sourcemap: true,
+        rollupOptions: {
+          onwarn(warning, defaultHandler) {
+            if (warning.code === 'SOURCEMAP_ERROR') {
+              return
+            }
+
+            defaultHandler(warning)
+          },
+        },
+    }
 })
